6def880706d39960442ac488697a7358974f7282
daniil <moiplansh028@gmail.com>
2022-02-01 16:29:22 +0300

    DI with static setters in context

--- src/main/java/ru/frolov/spring/MusicPlayer.java
+++ src/main/java/ru/frolov/spring/MusicPlayer.java
@@ -3,11 +3,38 @@ package ru.frolov.spring;
 public class MusicPlayer {
     private Music music;
 
+    private String name;
+    private int volume;
+
     public MusicPlayer(Music music){
         this.music = music;
     }
 
+    public MusicPlayer() {}
+
+
+    public void setMusic(Music music){
+        this.music = music;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public void playMusic(){
         System.out.println("Playing: "+music.getSong());
+    }
+
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public String getName() {
+        return name;
     }
 }
